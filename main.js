018be5a2093f155eeecd089ac4b3aea055f6cf2a
nailerHeum <kingman330@gmail.com>
2018-08-19 13:36:13 +0900

    about cookie

--- main.js
+++ main.js
@@ -5,11 +5,34 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var cookie = require('cookie');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
+function authIsOwner(req, res) {
+  var isOwner = false;
+  var cookies = {};
+  if (req.headers.cookie) { //expect no cookie case
+    cookies = cookie.parse(req.headers.cookie);
+  }
+  if(cookies.email === 'kingman330@gmail.com' && cookies.password === '111111') {
+    isOwner = true;
+  }
+  return isOwner;
+}
+
+function authStatusUI(req, res){
+  var authStatusUI = '<a href="/login">Login</a>'
+  if (authIsOwner(req, res) === true){
+    authStatusUI = '<a href="/logout_process">Logout</a>';
+  }
+  return authStatusUI;
+}
+
+var app = http.createServer((req,res) => {
+    var _url = req.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+    
+    
     if(pathname === '/'){
       if(queryData.id === undefined){
         fs.readdir('./data', function(error, filelist){
@@ -18,10 +41,11 @@ var app = http.createServer(function(request,response){
           var list = template.list(filelist);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+            `<a href="/create">create</a>`,
+            authStatusUI(req, res)
           );
-          response.writeHead(200);
-          response.end(html);
+          res.writeHead(200);
+          res.end(html);
         });
       } else {
         fs.readdir('./data', function(error, filelist){
@@ -40,10 +64,10 @@ var app = http.createServer(function(request,response){
                 <form action="delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
-                </form>`
+                </form>`, authStatusUI(req, res)
             );
-            response.writeHead(200);
-            response.end(html);
+            res.writeHead(200);
+            res.end(html);
           });
         });
       }
@@ -61,22 +85,27 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
-        response.writeHead(200);
-        response.end(html);
+        `, '', authStatusUI(req, res)
+        );
+        res.writeHead(200);
+        res.end(html);
       });
     } else if(pathname === '/create_process'){
+      if(authIsOwner(req, res) === false){
+        res.end(`Login required`)
+        return false;
+      }
       var body = '';
-      request.on('data', function(data){
+      req.on('data', function(data){
           body = body + data;
       });
-      request.on('end', function(){
+      req.on('end', function(){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
+            res.writeHead(302, {Location: `/?id=${title}`});
+            res.end();
           })
       });
     } else if(pathname === '/update'){
@@ -98,46 +127,103 @@ var app = http.createServer(function(request,response){
               </p>
             </form>
             `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+            authStatusUI(req, res)
           );
-          response.writeHead(200);
-          response.end(html);
+          res.writeHead(200);
+          res.end(html);
         });
       });
     } else if(pathname === '/update_process'){
       var body = '';
-      request.on('data', function(data){
+      req.on('data', function(data){
           body = body + data;
       });
-      request.on('end', function(){
+      req.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
           var title = post.title;
           var description = post.description;
           fs.rename(`data/${id}`, `data/${title}`, function(error){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
+              res.writeHead(302, {Location: `/?id=${title}`});
+              res.end();
             })
           });
       });
     } else if(pathname === '/delete_process'){
       var body = '';
-      request.on('data', function(data){
+      req.on('data', function(data){
           body = body + data;
       });
-      request.on('end', function(){
+      req.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
           var filteredId = path.parse(id).base;
           fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
+            res.writeHead(302, {Location: `/`});
+            res.end();
           })
       });
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
+    } else if (pathname === '/login') {
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Welcome';
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
+          `
+          <form action="/login_process" method="post"> 
+          <p>
+            <input type="text" name="email" placeholder="email">
+          </p>
+          <p>
+            <input type="password" name="password" placeholder="password">
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+          </form>
+          `,
+          `<a href="/create">create</a>`
+        );
+        res.writeHead(200);
+        res.end(html);
+      });
+    } else if (pathname === '/login_process'){
+      var body = '';
+      req.on('data', function(data){
+          body = body + data;
+      });
+      req.on('end', function(){
+        var post = qs.parse(body);
+        if(post.email === 'kingman330@gmail.com' && post.password === '111111'){
+          res.writeHead(302, {
+            'Set-Cookie': [
+              `email=${post.email}`,
+              `password=${post.password}`,
+              `nickname=heum`
+            ], 
+            Location: `/`
+          });
+          res.end();
+        } else {
+          res.end('who?');
+        }
+      });
+    } else if (pathname === '/logout_process') {
+      var post = qs.parse(body);
+        
+      res.writeHead(302, {
+        'Set-Cookie': [
+          `email=; Max-Age=0`,
+          `password=; Max-Age=0`,
+          `nickname=; Max-Age=0`
+        ], 
+        Location: `/`
+      });
+      res.end();
+    }else {
+      res.writeHead(404);
+      res.end('Not found');
     }
 });
 app.listen(3000);
